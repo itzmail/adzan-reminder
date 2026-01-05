21bc55e90bbf2e0b36225bc730acf688cc25e34f
It'smail <ismailnuralam@gmail.com>
2026-01-05 13:31:40 +0700

    Add Adzan Homebrew formula

---
+++ adzan.rb
@@ -0,0 +1,19 @@
+class Adzan < Formula
+  desc "Adzan reminder CLI with notification and adzan sound"
+  homepage "https://github.com/itzmail/adzan-reminder"
+  url "https://github.com/itzmail/adzan-reminder/releases/download/v0.1.0/adzan-macos"
+  sha256 "sha256:7b17e14801a3c4b23b19cdcdb054a7026b4427c9760016ea8f560b05bb1680b1"
+  version "0.1.0"
+
+  def install
+    bin.install "adzan-macos" => "adzan"
+
+    # Copy assets (suara adzan)
+    (bin/"../assets").mkpath
+    (bin/"../assets").install "assets/adzan_bedug.mp3"
+  end
+
+  test do
+    system "#{bin}/adzan", "--help"
+  end
+end
