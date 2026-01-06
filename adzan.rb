c8d80e84ae57c4f3bf02fd42525fa8de31ccf4d2
It'smail <ismailnuralam@gmail.com>
2026-01-06 10:18:55 +0700

    fix: fix asset suara_bedug

--- adzan.rb
+++ adzan.rb
@@ -1,7 +1,7 @@
 class Adzan < Formula
   desc "Adzan reminder CLI with notification and adzan sound"
   homepage "https://github.com/itzmail/adzan-reminder"
-  version "0.1.1"
+  version "0.1.2"
 
   if Hardware::CPU.intel?
     url "https://github.com/itzmail/adzan-reminder/releases/download/v0.1.2/adzan-macos-x86_64"
@@ -11,16 +11,25 @@ class Adzan < Formula
     sha256 "0a5bbde81f282669431129959164263ce56239b89908b786a78b9fceb12b4919"
   end
 
+  resource "adzan_sound" do
+    url "https://raw.githubusercontent.com/itzmail/adzan-reminder/main/assets/suara_bedug.mp3"
+    sha256 "ad06ceed5937b0e83a66dd2b8b33e139e787cf4fa4647921dac067a754623e6c"  # hash MP3-mu
+  end
+
   def install
     bin.install (Hardware::CPU.intel? ? "adzan-macos-x86_64" : "adzan-macos-arm64") => "adzan"
 
-    # Install suara adzan dari resource (handle nama file apapun dari GitHub)
+    # Buat folder assets
+    (bin/"../assets").mkpath
+
+    # Install suara adzan dari resource
     resource("adzan_sound").stage do
+      # Ambil file pertama (GitHub raw kadang kasih nama aneh)
       downloaded_file = Dir["*"].first
       if downloaded_file
         (bin/"../assets").install downloaded_file => "suara_bedug.mp3"
       else
-        raise "File adzan tidak ditemukan setelah download dari GitHub raw"
+        raise "File suara adzan tidak ditemukan setelah download"
       end
     end
   end
